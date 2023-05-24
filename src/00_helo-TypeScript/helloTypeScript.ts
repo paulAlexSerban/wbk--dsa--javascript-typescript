a60dea164d67e27b4637522be50c6d205b73b3b0
paulAlexSerban <paul.alex.serban@gmail.com>
2023-05-24 11:36:55 +0300

    adjusted tsconfig

--- src/00_helo-TypeScript/helloTypeScript.ts
+++ src/00_helo-TypeScript/helloTypeScript.ts
@@ -5,10 +5,10 @@
 
 // Declare a function called 'functionName' that takes a parameter called 'str' that is of type 'String'
 // The function will have a default value of "TypeScript" if no parameter is passed in
-function functionName(str: String = "TypeScript") {
-    // Print to the console
-    console.log("Hello " + str);
-}
+function greeting(parameterVariable: string = 'TypeScript' ) {
+    console.log(`Hello, World!`);
+    console.log(`${parameterVariable} is here, have no fear!!`);
+  }
 
-functionName();
-functionName("super JavaScript, my name is TypeScript");
+greeting();
+greeting('TypeScript');
